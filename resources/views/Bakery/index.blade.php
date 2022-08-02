bb2fa4619a2e857a9a6c0eaadf5a053f5ab4adb1
Matheus Andrade <matheusandrade009@gmail.com>
2022-08-02 19:17:59 -0300

    fix: add cart error 6 test url

--- resources/views/Bakery/index.blade.php
+++ resources/views/Bakery/index.blade.php
@@ -388,7 +388,7 @@
 
             function AddCart(id) {
                 $.ajax({
-                    url: `{{ asset('adicionar-carrinho/${id}') }}`,
+                    url: `{{ asset('https://cantinho-doce.herokuapp.com/adicionar-carrinho/${id}') }}`,
                     type: "GET",
                 }).done(function(response) {
                     console.log(response);
