2caee26587bd28174c50a08e055a3a30b97ba397
Matheus Andrade <matheusandrade009@gmail.com>
2022-08-02 22:54:34 -0300

    fix: update and delete on cascade product_id

--- database/migrations/2022_02_26_140820_create_favorites_table.php
+++ database/migrations/2022_02_26_140820_create_favorites_table.php
@@ -16,10 +16,10 @@ class CreateFavoritesTable extends Migration
         Schema::create('favorites', function (Blueprint $table) {
             $table->id();
             $table->integer('user_id');
-            $table->integer('product_id')->unsigned()->nullable();
+            $table->integer('product_id')->unsigned();
             $table->timestamps();
 
-            $table->foreign('product_id')->references('id')->on('products');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
         });
     }
 
